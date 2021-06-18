13110cd0cd2023b6d0da255cb3307ce131c78f13
David Křístek <david.kristek05@gmail.com>
2021-06-18 20:19:22 +0200

    test

--- server.js
+++ server.js
@@ -1,57 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const app = express();
+const http = require('http');
 
-const checkAuth = require("./lib/chectAuth");
-const isAdmin = require("./lib/isAdmin");
-const userAdmin = require('./controllers/Admin/UserAdminController');
-const http = require("http").Server(app);
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "*",
-  },
+const port=process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-dotenv.config();
-const cors = require("cors");
-const morgan = require("morgan");
-
-const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.DB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    console.log("Database connected");
-    http.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  })
-  .catch((err) => console.log(err));
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-// sockets :
-require("./Sockets")(io);
-
-const test = () => {
-  console.log("healllo");
-};
-app.get("/api/admin/first", checkAuth, userAdmin.first_admin, require("./routes/admin"));
-
-
-app.use("/api/category", require("./routes/category"));
-app.use("/api/posts", require("./routes/posts"));
-
-
-// first admin route
-app.get("/api/admin/first", checkAuth, userAdmin.first_admin, require("./routes/admin"));
-
-app.use("/api/admin", checkAuth, isAdmin, require("./routes/admin"));
-
-app.get("/", (req, res) => {
-  res.json({"msg" : "Welcome to server created by David Kristek"})
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
-
